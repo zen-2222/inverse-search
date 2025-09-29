98ebba23030cc215962e57680edf2f86bad67fb8
M <www.zem21@gmail.com>
2025-09-30 00:19:27 +0300

    fix searching & adding

--- inv.cpp
+++ inv.cpp
@@ -7,12 +7,20 @@
 #include <vector>
 
 void add (std::ifstream &file,std::filesystem::path &dir, std::unordered_map <std::string, std::vector<std::pair <std::filesystem::path,int>>> &hash){
-    std::string word;
+    std::string word; 
 
-    while(file >> word){
-    if(hash.find(word) != hash.end()){hash.at(word).back().second++; continue;}
-                hash[word].push_back({dir.filename(),1});
-}
+    while(file >> word){ bool z=1;
+        if(hash.find(word) != hash.end()){
+            for(auto &k: hash.at(word)){
+                if(!k.first.filename().string().compare(dir.filename().string())) {k.second++;z=0; break;} //if pair is found increment counter and move on
+
+
+            }
+            
+    
+        }
+        if(z) hash[word].push_back({dir.filename().string(),1}); // if the word exists but/or if the pair doesn't exist
+    }
     file.close();
 
 }
@@ -21,11 +29,9 @@ void add (std::ifstream &file,std::filesystem::path &dir, std::unordered_map <st
 
 
 void search (std::string &word,std::unordered_map <std::string, std::vector<std::pair <std::filesystem::path,int>>> &hash){ 
-    //deal with exception later
   
-    for(auto &s: hash.at(word)){
-        std::cout<<"{"<<s.first.string()<<","<<s.second<<"}  ";
-    }
+    for(auto s: hash.at(word)){
+        std::cout<<"{"<<s.first.filename().string()<<","<<s.second<<"} ";  }
    
     std::cout<<"\n";
 }
@@ -42,38 +48,44 @@ for(auto &k:hash){
 }
 
 
-int main(){while(true){
-    std::unordered_map <std::string, std::vector<std::pair <std::filesystem::path,int>>> hash;
-    std::cout<<"Choose:\n1.Add file\n2.Search word\n3.Delete file\n4.Exit\n";
-    int x;        std::string s;std::ifstream file;std::filesystem::path p;
-    std::cin>>x;
-    switch(x){
-        case 1: 
-            std::cout<<"Enter directory (or 0 to return):";
-            std::cin>>s;
-            if(!s.compare("0")) continue;
-            file.open(s,std::ios::in);
-            if(!file.is_open()){std::cout<<"File not found!\n";continue;}
-            p = s;
-            add(file,p,hash);
-        break;
+int main(){  std::unordered_map <std::string, std::vector<std::pair <std::filesystem::path,int>>> hash;
+    while(true){
+  
+        std::cout<<"Choose:\n1.Add file\n2.Search word\n3.Delete file\n4.Exit\n";
+        int x; std::string s;std::ifstream file;std::filesystem::path p;
+        std::cin>>x;
 
-        case 2:
-            std::cout<<"Enter word:\n";
-            std::cin>>s;
-            if(hash.find(s) ==hash.end()){ std::cout<<"Word not found!\n";continue;}
-        break;
+        switch(x){
+            case 1: 
+                std::cout<<"Enter directory (or 0 to return):";
+                std::cin>>s;
+                if(!s.compare("0")) continue;
+                file.open(s,std::ios::in);
+                if(!file.is_open()){std::cout<<"File not found!\n";continue;}
+                p = s;
+                add(file,p,hash);
+            break;
 
-        case 3:
-            std::cout<<"Enter file name with .txt:\n";
-            std::cin>>s;
-            deletes(s,hash);
+            case 2:
+                std::cout<<"Enter word:\n";
+                std::cin>>s;
+                if(hash.find(s) ==hash.end()){ std::cout<<"Word not found!\n";continue;}
+                search(s,hash);
+            break;
 
-        break;
-}
+            case 3:
+                std::cout<<"Enter file name with .txt:\n";
+                std::cin>>s;
+                deletes(s,hash);
+
+            break;
+        
+            case 4:return 0;
+
+        }
 
 
-}
+    }
 
 
 return 0;}
