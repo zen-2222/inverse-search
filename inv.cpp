47562095afe7f2a3b916aadc7d98f6a7c4d6da55
M <www.zem21@gmail.com>
2025-09-29 20:26:41 +0300

    add delete

--- inv.cpp
+++ inv.cpp
@@ -21,11 +21,25 @@ void add (std::ifstream file,std::filesystem::path &dir, std::unordered_map <std
 
 
 
-void search (std::string &word,std::unordered_map <std::string, std::vector<std::pair <std::filesystem::path,int>>> &hash){
-    
+void search (std::string &word,std::unordered_map <std::string, std::vector<std::pair <std::filesystem::path,int>>> &hash){ 
+    //deal with exception later
+  
     for(auto &s: hash.at(word)){
         std::cout<<"{"<<s.first<<","<<s.second<<"}  ";
     }
+   
+    std::cout<<"\n";
+}
+
+void deletes (std::string &word,std::unordered_map <std::string, std::vector<std::pair <std::filesystem::path,int>>> &hash){
+for(auto &k:hash){
+
+    for(int i=0;i<k.second.size();i++){
+        if(k.second.at(i).first.filename() == word) {k.second.erase(k.second.begin() + i); i--;}
+    }
+
+}
+
 }
 
 
